f2b331a5d7c65ef6c943f491511380c5796ec819
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-03 12:35:44 +0900

    corsで2つのURLを許可
    vercelでdeployブランチの変更も確認できるように。

--- HAKUbackend/config/cors.php
+++ HAKUbackend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL'), env('FRONTEND_URL_DEV')],
 
     'allowed_origins_patterns' => [],
 
